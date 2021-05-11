f07c76c429314d2b2412162f2a05f68fe2dd7015
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2021-05-11 19:37:19 -0400

    ListCard layout

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
+import { Button, Card, Col, Row } from 'react-bootstrap';
+
+import EditIcon from '@material-ui/icons/Edit';
+
+
 function TaskCard() {
     return (
-        <div>
-            
-        </div>
+        <Card className="my-1">
+            <Card.Body>
+                <Row className="d-flex justify-content-between align-middle">
+                    <Col>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                            <label class="form-check-label" for="flexCheckDefault">
+                                Estudar React
+                            </label>
+                        </div>
+                    </Col>
+                    <Col className="d-flex justify-content-end">
+                        <Button className="py-0 border-0 bg-body text-dark">
+                            <EditIcon/>
+                        </Button>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
     )
 }
 
