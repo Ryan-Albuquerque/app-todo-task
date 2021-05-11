08cbf2d80289821e00ffb0d0295483aa95f6365e
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2021-05-11 19:03:05 -0400

    TaskCard component and change React importation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import {React} from 'react'
+import React from 'react'
 import {Container, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
--- src/components/ListTask/index.js
+++ src/components/ListTask/index.js
@@ -1,8 +1,10 @@
-import {React} from 'react';
+import React from 'react';
 
-import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 import AddIcon from '@material-ui/icons/Add';
+
+import TaskCard from '../TaskCard';
 
 function ListTask (){
     return (
@@ -18,8 +20,7 @@ function ListTask (){
 
             <Row>
                 <Container className="bg-light border border-light border-4 rounded my-3 p-3 ">
-
-                    
+                    <TaskCard />
                 </Container>
             </Row>
         </Container>
