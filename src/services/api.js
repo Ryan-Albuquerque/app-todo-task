b6d1e6611854bff08e0c39e49e3ddf74d6ad8a98
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2021-05-11 21:01:44 -0400

    getErrorMessage

--- src/services/api.js
+++ src/services/api.js
@@ -6,4 +6,18 @@ const api = axios.create({
     baseURL: config.apiUrl
 });
 
+export const getErrorMessage = (error) => {
+    try {
+        if (error?.response?.data?.message) 
+            return error.response.data.message;
+        if (error?.response?.status === 404)
+            return 'Link para o Servidor não encontrado(404)';
+        if (error?.response?.status === 500)
+            return 'Tivemos um problema com o servidor, entre em contato!';
+    } catch (erro) {
+        return 'Erro Desconhecido';
+    }
+  };
+  
+
 export default api;
