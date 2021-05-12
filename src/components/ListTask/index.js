900006e34a13be65ac5c3ba0c08790422608fa6a
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2021-05-11 21:02:12 -0400

    list tasks

--- src/components/ListTask/index.js
+++ src/components/ListTask/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Button, Col, Container, Row } from 'react-bootstrap';
 
 import AddIcon from '@material-ui/icons/Add';
 
+import api, { getErrorMessage } from '../../services/api';
 import TaskCard from '../TaskCard';
 
+import Notification from '../../utils/Notification'
+import Constants from '../../utils/constants';
+
 function ListTask (){
+
+    const [taskList, setTaskList] = useState([]);
+
+    useEffect(() => {
+
+        async function fetchData(){
+            try {
+                const response = await api.get('/tasks');
+
+                setTaskList(response.data.tasks);
+            } catch (error) {
+                const message = getErrorMessage(error);
+                Notification(Constants.Notification.types.error, message)
+            }      
+        }
+
+        fetchData();
+    }, [])
+
     return (
         <Container className="my-3">
             <Row>
@@ -20,7 +43,9 @@ function ListTask (){
 
             <Row>
                 <Container className="bg-light border border-light border-4 rounded my-3 p-3 ">
-                    <TaskCard />
+                    {taskList?.map((task)=>{
+                        <TaskCard data={task} />
+                    })}
                 </Container>
             </Row>
         </Container>
